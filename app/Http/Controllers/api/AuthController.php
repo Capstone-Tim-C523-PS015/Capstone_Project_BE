5a48e23bf4adce0df1587aa4a60ec40c93dcfcce
gunzxx <ggm5850@gmail.com>
2023-11-21 11:39:46 +0700

    api logout done

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\api;
 
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Database\QueryException;
@@ -12,7 +13,7 @@ class AuthController extends Controller
 {
     function login(Request $request) {
         if(auth()->guard('api')->check()){
-            return response()->json(['message' => 'Kamu sudah login'], 400);
+            return response()->json(['message' => 'kamu sudah login'], 400);
         }
 
         $validator = Validator::make($request->all(),[
@@ -20,25 +21,25 @@ class AuthController extends Controller
             'password' => 'required',
         ]);
         if($validator->fails()){
-            return response()->json(['message' => 'Data tidak valid'], 400);
+            return response()->json(['message' => 'data tidak valid'], 400);
         }
 
         $creadential = $request->only(['email', 'password']);
         $token = auth()->guard('api')->attempt($creadential);
 
         if(!$token){
-            return response()->json(['message' => 'Username atau password salah'], 401);
+            return response()->json(['message' => 'username atau password salah'], 401);
         }
 
         return response()->json([
-            'message' => 'Login berhasil',
+            'message' => 'login berhasil',
             'token' => $token,
         ]);
     }
 
     function register(Request $request) {
         if(auth()->guard('api')->check()){
-            return response()->json(['message' => 'Kamu sudah login'], 400);
+            return response()->json(['message' => 'kamu sudah login'], 400);
         }
 
         try{
@@ -49,7 +50,7 @@ class AuthController extends Controller
             ]);
 
             if($validator->fails()){
-                return response()->json(['message' => 'Data tidak valid'], 400);
+                return response()->json(['message' => 'data tidak valid'], 400);
             }
 
             $user = User::create([
@@ -59,18 +60,33 @@ class AuthController extends Controller
             ]);
 
             return response()->json([
-                'message' => 'Register berhasil',
+                'message' => 'register berhasil',
                 'user' => $user,
             ], 201);
         }
         catch(QueryException $e){
             $message = 'Query error';
             if($e->errorInfo[1] == 1062){
-                $message = 'Email sudah digunakan';
+                $message = 'email sudah digunakan';
             }
             return response()->json([
                 'message' => $message,
             ],400);
         }
+    }
+
+    function logout(Request $request) {
+        if(!$request->bearerToken()){
+            return response()->json([
+                'message' => 'token diperlukan',
+            ],401);
+        }
+
+        if(!auth()->guard('api')->check()){
+            return response()->json(['message' => 'anda sudah logout/token tidak valid']);
+        }
+
+        Auth::guard('api')->logout();
+        return response()->json(['message' => 'logout berhasil']);
     }
 }
