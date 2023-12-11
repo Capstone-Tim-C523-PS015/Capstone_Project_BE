49795fe707744664a210801273011f66ad6fd017
gunzxx <ggm5850@gmail.com>
2023-12-11 16:22:21 +0700

    add update status jobs

---
+++ app/Http/Controllers/JobsController.php
@@ -0,0 +1,25 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Todo;
+use Illuminate\Support\Carbon;
+
+class JobsController extends Controller
+{
+    function updateStatus() {
+        $kemarin = Todo::whereDate('deadline', Carbon::now()->subDay())->where(['status'=>'dikerjakan'])->get();
+        $kemarin->each(function($todo){
+            $todo->update(['status' => 'terlambat']);
+        });
+        
+        $sekarang = Todo::whereDate('deadline', Carbon::now())->where(['status'=>'menunggu'])->get();
+        $sekarang->each(function($todo){
+            $todo->update(['status' => 'dikerjakan']);
+        });
+        
+        return response()->json([
+            'message' => 'data berhasil diperbarui',
+        ]);
+    }
+}
