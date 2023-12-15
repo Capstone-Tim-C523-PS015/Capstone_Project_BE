2256de634208050277be4166253ee2e6d38e7438
gunzxx <ggm5850@gmail.com>
2023-12-15 13:12:39 +0700

    update jobs controller

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Carbon;
 class JobsController extends Controller
 {
     function updateStatus() {
-        $kemarin = Todo::where('deadline', '<', Carbon::now())->where(['status'=>'dikerjakan'])->get(['id','status']);
+        $kemarin = Todo::where('deadline', '<', Carbon::now())->where('status',['dikerjakan','menunggu'])->get(['id','status']);
         $kemarin->each(function($todo){
             $todo->update(['status' => 'telat']);
         });
