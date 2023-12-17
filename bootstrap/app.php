ea8a1a369c4a4bb61a9a85afae9b7a24a8e1fc61
gunzxx <ggm5850@gmail.com>
2023-12-18 01:41:11 +0700

    update cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -80,8 +80,10 @@ $app->routeMiddleware([
     'auth' => App\Http\Middleware\Authenticate::class,
 ]);
 $app->middleware([
-    App\Http\Middleware\CorsMiddleware::class
+    App\Http\Middleware\CorsMiddleware::class,
+    Nord\Lumen\Cors\CorsMiddleware::class,
 ]);
+$app->register(Nord\Lumen\Cors\CorsServiceProvider::class);
 $app->register(App\Providers\CatchAllOptionsRequestsProvider::class);
 
 /*
