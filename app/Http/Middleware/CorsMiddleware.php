d9e23fc7488edf01b2b28dd2e4d30ec754be34c8
gunzxx <ggm5850@gmail.com>
2023-12-18 01:46:10 +0700

    update cors credentials

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -18,7 +18,7 @@ class CorsMiddleware
         $headers = [
             'Access-Control-Allow-Origin'      => '*',
             'Access-Control-Allow-Methods'     => 'POST, GET, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Allow-Credentials' => 'false',
             'Access-Control-Max-Age'           => '86400',
             // 'Access-Control-Allow-Headers'     => 'Content-Type, Authorization, X-Requested-With'
         ];
