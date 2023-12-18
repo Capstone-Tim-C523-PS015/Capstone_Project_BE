a2505a803d1b2c144ce2bb3dda78810fbcb3205e
gunzxx <ggm5850@gmail.com>
2023-12-18 07:41:01 +0700

    update cors

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -90,6 +90,9 @@ class AuthController extends Controller
         }
 
         Auth::logout();
+        // header('Access-Control-Allow-Origin: *');
+        // header('Access-Control-Allow-Methods: *');
+        // header('Access-Control-Allow-Headers: *');
         return response()->json(['message' => 'logout berhasil']);
     }
 }
