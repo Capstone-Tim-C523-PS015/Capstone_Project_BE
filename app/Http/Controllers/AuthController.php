3f9c6044b45a5b732b55596d5543be36253d41e5
gunzxx <ggm5850@gmail.com>
2023-12-18 09:13:39 +0700

    update api

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -89,7 +89,7 @@ class AuthController extends Controller
             return response()->json(['message' => 'token tidak valid'], 400);
         }
 
-        Auth::logout();
+        Auth::guard('api')->logout();
         // header('Access-Control-Allow-Origin: *');
         // header('Access-Control-Allow-Methods: *');
         // header('Access-Control-Allow-Headers: *');
