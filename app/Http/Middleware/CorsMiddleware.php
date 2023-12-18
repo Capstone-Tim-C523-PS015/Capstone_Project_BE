a3f7deada608cb5a24bdc23280d7cbf98ce15cb5
gunzxx <ggm5850@gmail.com>
2023-12-18 08:56:29 +0700

    update cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -19,15 +19,15 @@ class CorsMiddleware
         $headers = [
             'Access-Control-Allow-Origin'      => '*',
             'Access-Control-Allow-Methods'     => 'GET, POST, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Credentials' => 'true',
-            'Access-Control-Max-Age'           => '86400',
+            // 'Access-Control-Allow-Credentials' => 'true',
+            // 'Access-Control-Max-Age'           => '86400',
             // 'Access-Control-Allow-Headers'     => 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range'
         ];
 
-        // if ($request->isMethod('OPTIONS'))
-        // {
-        //     return response()->json('{"method":"OPTIONS"}', 200, $headers);
-        // }
+        if ($request->isMethod('OPTIONS'))
+        {
+            return response()->json('{"method":"OPTIONS"}', 200, $headers);
+        }
 
         $response = $next($request);
         foreach($headers as $key => $value)
