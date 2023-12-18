33b63e5b29f2497b1e651f233e15e6be7203d95a
gunzxx <ggm5850@gmail.com>
2023-12-18 08:46:24 +0700

    update cors

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -15,26 +15,27 @@ class CorsMiddleware
      */
     public function handle($request, Closure $next)
     {
-        $headers = [
-            'Access-Control-Allow-Origin'      => '*',
-            'Access-Control-Allow-Methods'     => 'GET, POST, OPTIONS, PUT, DELETE',
-            'Access-Control-Allow-Credentials' => 'true',
-            'Access-Control-Max-Age'           => '86400',
-            // 'Access-Control-Allow-Headers'     => 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range'
-        ];
+        return $next($request)->header('Access-Control-Allow-Origin', '*')->header('Access-Control-Allow-Methods','GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS');
+        // $headers = [
+        //     'Access-Control-Allow-Origin'      => '*',
+        //     'Access-Control-Allow-Methods'     => 'GET, POST, OPTIONS, PUT, DELETE',
+        //     'Access-Control-Allow-Credentials' => 'true',
+        //     'Access-Control-Max-Age'           => '86400',
+        //     // 'Access-Control-Allow-Headers'     => 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range'
+        // ];
 
-        if ($request->isMethod('OPTIONS'))
-        {
-            return response()->json('{"method":"OPTIONS"}', 200, $headers);
-        }
+        // if ($request->isMethod('OPTIONS'))
+        // {
+        //     return response()->json('{"method":"OPTIONS"}', 200, $headers);
+        // }
 
-        $response = $next($request);
-        foreach($headers as $key => $value)
-        {
-            $response->header($key, $value);
-        }
+        // $response = $next($request);
+        // foreach($headers as $key => $value)
+        // {
+        //     $response->header($key, $value);
+        // }
 
-        // return response()->json($response);
-        return $response;
+        // // return response()->json($response);
+        // return $response;
     }
 }
