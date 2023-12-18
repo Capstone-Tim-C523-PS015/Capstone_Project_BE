569b78ef4c73b975891a2e8bfce403cb5b83c1a5
gunzxx <ggm5850@gmail.com>
2023-12-18 13:16:11 +0700

    update mail

--- app/Http/Controllers/ResetPasswordController.php
+++ app/Http/Controllers/ResetPasswordController.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -10,27 +11,28 @@ use Illuminate\Support\Facades\Validator;
 
 class ResetPasswordController extends Controller
 {
-    function email(Request $request) {
-        $validate = Validator::make($request->all(),[
+    function email(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
             'email' => 'required|email',
         ]);
-        if($validate->fails()){
-            return response()->json(['message' => 'data tidak valid'],400);
+        if ($validate->fails()) {
+            return response()->json(['message' => 'data tidak valid'], 400);
         }
-        
+
         $email = $request->email;
         $user = User::where(['email' => $email])->get();
-        if($user->count() <= 0){
-            return response()->json(['message' => 'user tidak ditemukan'],400);
+        if ($user->count() <= 0) {
+            return response()->json(['message' => 'user tidak ditemukan'], 400);
         }
         $cekToken = Token::where(['email' => $email])->get();
-        if($cekToken->count() > 0){
-            return response()->json(['message' => 'email verifikasi sudah dikirim, harap cek spam jika email tidak ada'],400);
+        if ($cekToken->count() > 0) {
+            return response()->json(['message' => 'email verifikasi sudah dikirim, harap cek spam jika email tidak ada'], 400);
         }
-        
+
         $token = bin2hex(random_bytes(32));
         try {
-            Mail::send('email', ['data' => 'hello world', 'token' => $token], function($message) use ($email){
+            Mail::send('email', ['data' => 'hello world', 'token' => $token], function ($message) use ($email) {
                 $message->subject('Reset Password - PlanPlan');
                 $message->to($email);
             });
@@ -40,48 +42,48 @@ class ResetPasswordController extends Controller
             ]);
             return response()->json(['message' => 'email berhasil dikirim, cek spam apabila email tidak tersedia', 'token' => $token]);
         } catch (\Throwable $th) {
-            return response()->json(['message' => 'email gagal dikirim', 'error' => $th->getMessage()], 400);
+            return response()->json(['message' => 'email gagal dikirim', 'error' => $th], 400);
         }
     }
 
-    function verifyToken(Request $request) {
-        $validate = Validator::make($request->all(),[
+    function verifyToken(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
             'token' => 'required',
         ]);
-        if($validate->fails()){
-            return response()->json(['message' => 'data tidak valid'],400);
+        if ($validate->fails()) {
+            return response()->json(['message' => 'data tidak valid'], 400);
         }
 
         $token = $request->token;
         $cekToken = Token::where(['token' => $token])->get();
-        if($cekToken->count() == 1){
+        if ($cekToken->count() == 1) {
             return response()->json(['message' => 'token valid']);
-        }
-        else{
-            return response()->json(['message' => 'token tidak valid'],400);
+        } else {
+            return response()->json(['message' => 'token tidak valid'], 400);
         }
     }
 
-    function updatePassword(Request $request) {
-        $validate = Validator::make($request->all(),[
+    function updatePassword(Request $request)
+    {
+        $validate = Validator::make($request->all(), [
             'new_password' => 'required|confirmed|min:3',
             'new_password_confirmation' => 'required',
             'token' => 'required',
         ]);
-        if($validate->fails()){
-            return response()->json(['message' => 'data tidak valid', 'data' => $request->all()],400);
+        if ($validate->fails()) {
+            return response()->json(['message' => 'data tidak valid', 'data' => $request->all()], 400);
         }
 
         $token = $request->token;
         $cekToken = Token::where(['token' => $token])->get();
-        if($cekToken->count() < 1){
-            return response()->json(['message' => 'token tidak valid'],400);
-        }
-        else{
+        if ($cekToken->count() < 1) {
+            return response()->json(['message' => 'token tidak valid'], 400);
+        } else {
             $cekToken = $cekToken->first();
             $user = User::where(['email' => $cekToken->email])->get()->first();
-            if($user->count()<1){
-                return response()->json(['message' => 'user tidak ditemukan'],404);
+            if ($user->count() < 1) {
+                return response()->json(['message' => 'user tidak ditemukan'], 404);
             }
 
             $user->update([
