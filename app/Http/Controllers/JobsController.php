42560247f5153f72ff1d5b387a7d47ee9847aaee
gunzxx <ggm5850@gmail.com>
2023-12-18 10:49:41 +0700

    update todoo

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -25,6 +25,7 @@ class JobsController extends Controller
         
         return response()->json([
             'message' => 'data berhasil diperbarui',
+            'todo' => Todo::find(27),
             'data' => Carbon::today(),
         ]);
     }
